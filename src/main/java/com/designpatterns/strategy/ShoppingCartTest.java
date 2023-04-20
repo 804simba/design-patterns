5716468b535fb1af958dc15464eafd48d78b91d2
timolisa <timothyolisaeloka@gmail.com>
2023-04-20 16:32:21 +0100

    add strategy design pattern

--- src/main/java/com/designpatterns/strategy/ShoppingCartTest.java
+++ src/main/java/com/designpatterns/strategy/ShoppingCartTest.java
@@ -1,6 +1,8 @@
 package com.designpatterns.strategy;
 
 public class ShoppingCartTest {
+    // Strategy pattern is useful when we have multiple algorithms for specific task,
+    // and we want our application to be flexible to chose any of the algorithm at runtime for specific task.
     public static void main(String[] args) {
         ShoppingCart cart = new ShoppingCart();
 
